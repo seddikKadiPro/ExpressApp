b02a6312cef43f982b10d9e418e869c3bba8a007
kadiseddikPro <seddikKadiPro.com>
2019-02-08 17:25:43 +0100

    setting up the vuetify module

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,13 +1,25 @@
 <template>
-  <div>
-    <h1>Register</h1>
-    <input type="email" name="email" placeholder="email" v-model="email">
-    <input type="password" name="password" placeholder="password" v-model="password">
-    <div class="error" v-html="error">
-      
-    </div>
-    <button @click="register">Register</button>
- </div>
+  <v-layout column>
+    <v-flex>
+      <div class="white elevation-2 ">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>
+            Register
+          </v-toolbar-title>
+        </v-toolbar>
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <input type="email" name="email" placeholder="email" v-model="email">
+          <br>
+          <input type="password" name="password" placeholder="password" v-model="password">
+          <br>
+          <div class="error" v-html="error">
+          </div>
+          <v-btn class="cyan" @click="register">Register</v-btn>
+          <br>
+         </div>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 <script>
 import AuthenticationService from '@/services/AuthenticationService'
@@ -22,19 +34,15 @@ export default {
   },
   methods: {
     async register () {
-      try{
+      try {
+        // eslint-disable-next-line
         const response = await AuthenticationService.register({
           email: this.email,
-          password: this.password,
-          
-         })
-
-      }catch(error){
-
-        this.error=error.response.data.error
-
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
       }
-      
     }
   }
 }
@@ -42,6 +50,6 @@ export default {
 <style scoped>
 .error{
   color: red;
-  
+
 }
 </style>
